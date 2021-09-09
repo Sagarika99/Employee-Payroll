2732f87f37104d3241b3bcafac4a660b2f222980
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-09-10 01:06:01 +0530

    created Watch service to eatch directory

--- lib/src/test/java/com/bridgelabz/employeepayrolltest/EmployeePayrollTest.java
+++ lib/src/test/java/com/bridgelabz/employeepayrolltest/EmployeePayrollTest.java
@@ -59,6 +59,13 @@ public class EmployeePayrollTest {
 		//List files, directories as well as files with extension
 		Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
 		Files.newDirectoryStream(playPath).forEach(System.out::println);
-		//Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp")).forEach(System.out::println);
+		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp")).forEach(System.out::println);
+	}
+	
+	@Test
+	public void givenDirectoryWhenWatchedListAllTheACtivities() throws IOException{
+		Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+		Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+		new Java8WatchServiceExample(dir).processEvents();
 	}
 }
